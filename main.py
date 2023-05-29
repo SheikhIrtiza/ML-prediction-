9894758eea660cc494eece1cf91d4a65be7c8f3c
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-29 23:20:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas 
 from sklearn.linear_model import LinearRegression
 data = pandas.read_csv('iphone_price.csv')
-model = LinearRegression()
+model = LinearRegression() 
 model.fit(data[['version']], data[['price']])
 print(model.predict([[14]]))
 print(model.predict([[30]])) 
